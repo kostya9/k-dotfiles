0925189c1e59fd16ef029f9afdcbc5b351047d07
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-07-29 00:00:58 +0300

    Fix apply configs script

--- apply-configs.ps1
+++ apply-configs.ps1
@@ -2,5 +2,7 @@ cp "rider/.ideavimrc" ~
 
 New-Item -ItemType Directory -Force -Path "~/nvim/.config" | Out-Null
 if(!(Test-Path -Path "~/.config/nvim")) {
-	New-Item -ItemType SymbolicLink -Path "nvim/.config/nvim" -Target "~/.config/nvim"
+	$currentDirectory = Get-Location
+	$nvimDirectory = Join-Path $currentDirectory "nvim/.config/nvim"
+	New-Item -ItemType SymbolicLink -Target $nvimDirectory -Path "~/.config/nvim" | Out-Null
 }
