9f7d3daa5f38648a0656f8604bb4e17250d14df9
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-07-29 00:13:31 +0300

    Clean configs before applying

--- apply-configs.ps1
+++ apply-configs.ps1
@@ -1,10 +1,10 @@
+./clean-configs.ps1
+
 $currentDirectory = Get-Location
 
 $ideavimrcLocation = Join-Path $currentDirectory "rider/.ideavimrc"
 New-Item -ItemType SymbolicLink -Target $ideavimrcLocation -Path "~/.ideavimrc" | Out-Null
 
 New-Item -ItemType Directory -Force -Path "~/nvim/.config" | Out-Null
-if(!(Test-Path -Path "~/.config/nvim")) {
-	$nvimDirectory = Join-Path $currentDirectory "nvim/.config/nvim"
-	New-Item -ItemType SymbolicLink -Target $nvimDirectory -Path "~/.config/nvim" | Out-Null
-}
+$nvimDirectory = Join-Path $currentDirectory "nvim/.config/nvim"
+New-Item -ItemType SymbolicLink -Target $nvimDirectory -Path "~/.config/nvim" | Out-Null
