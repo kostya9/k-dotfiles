5d7316e530e5c57bf198f9159a480b6214198a13
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-08-13 17:50:15 +0300

    Add highlights to rider and make Enter stop them

--- nvim/.config/nvim/lua/k/remap.lua
+++ nvim/.config/nvim/lua/k/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- This unsets the "last search pattern" register by hitting return
+vim.keymap.set({ "n", "v" }, "<CR>", ":nohlsearch<CR><CR>", { silent = true })
