91e10746ffa8d3b857cc37a437a133f3822b4963
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-12-23 19:05:10 +0200

    Fix config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -9,7 +9,8 @@ lsp.ensure_installed({
 	'omnisharp'
 	, 'lemminx',
 	'prismals',
-	'tailwindcss'
+	'tailwindcss',
+	'lua_ls'
 })
 
 local cmp = require('cmp')
@@ -22,6 +23,33 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 })
 
 local lspconfig = require'lspconfig'
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 lspconfig.lemminx.setup {
 	filetypes ={"xml", "xsd", "xsl", "xslt", "svg", "axaml", "xaml"}
--- nvim/.config/nvim/lua/k/packer.lua
+++ nvim/.config/nvim/lua/k/packer.lua
@@ -3,8 +3,7 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-	-- Packer can manage itself
+return require('packer').startup(function(use) 
 	use 'wbthomason/packer.nvim'
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
@@ -12,41 +11,38 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-		vim.cmd('colorscheme rose-pine');
-	end
-})
+	use { 'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine') end }
 
-use({'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}})
+	use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
 
-use('ThePrimeagen/harpoon')
+	use {'ThePrimeagen/harpoon', requires = { {"nvim-lua/plenary.nvim"} }}
 
-use('mbbill/undotree')
+	use 'mbbill/undotree'
 
-use('tpope/vim-fugitive')
+	use 'tpope/vim-fugitive'
 
-use {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v2.x',
-	requires = {
-		-- LSP Support
-		{'neovim/nvim-lspconfig'},             -- Required
-		{                                      -- Optional
-		'williamboman/mason.nvim',
-		run = function()
-			pcall(vim.cmd, 'MasonUpdate')
-		end,
-	},
-	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	-- Autocompletion
-	{'hrsh7th/nvim-cmp'},     -- Required
-	{'hrsh7th/cmp-nvim-lsp'}, -- Required
-	{'L3MON4D3/LuaSnip'},     -- Required
-	{'hrsh7th/cmp-nvim-lua'},
-}
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+		{'hrsh7th/cmp-nvim-lua'}
+	}
 }
 
-use('Hoffs/omnisharp-extended-lsp.nvim')
+use 'Hoffs/omnisharp-extended-lsp.nvim'
 
 end)
