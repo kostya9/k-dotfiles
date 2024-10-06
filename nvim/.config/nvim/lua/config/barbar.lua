f985bf2eb3d408a5d0711bbd77a6658bc10c14d8
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2024-10-06 23:13:02 +0300

    Add surround plugin. Add hotkey for barbar close everything but

--- nvim/.config/nvim/lua/config/barbar.lua
+++ nvim/.config/nvim/lua/config/barbar.lua
@@ -2,5 +2,6 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-h>', '<Cmd>BufferPrevious<CR>', opts)
 vim.keymap.set('n', '<C-l>', '<Cmd>BufferNext<CR>', opts)
 vim.keymap.set('n', '<C-q>', '<Cmd>BufferClose<CR>', opts)
+vim.keymap.set('n', '<C-a><C-q>', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
 vim.keymap.set('n', '<leader>q', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
 
--- nvim/.config/nvim/lua/config/rose-pine.lua
+++ nvim/.config/nvim/lua/config/rose-pine.lua
@@ -1,12 +1,11 @@
 require("rose-pine").setup({
     variant = "auto", -- auto, main, moon, or dawn
     dark_variant = "main", -- main, moon, or dawn
-    dim_inactive_windows = false,
+    dim_inactive_windows = true,
     extend_background_behind_borders = true,
 
     enable = {
         terminal = true,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
         migrations = true, -- Handle deprecated options automatically
     },
 
