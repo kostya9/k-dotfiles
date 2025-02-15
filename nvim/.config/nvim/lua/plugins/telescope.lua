3f8ce8c3d904905a07c3e632711352fbb992fb01
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2025-02-16 00:39:27 +0200

    Allow searching in neovim config folder

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,22 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-dap.nvim' }
+	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-dap.nvim' },
+	opts =
+	{
+		defaults = {
+			vimgrep_arguments =
+			{
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--ignore-file",
+				"~/.config/nvim/.ignore",
+			}
+		}
+	}
 }
